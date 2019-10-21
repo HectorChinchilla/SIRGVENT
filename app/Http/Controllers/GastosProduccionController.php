66e2a07888f955108ae35fe28202fa7c0b421cd3
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-21 16:23:06 -0600

    Se creó el controlador GastosProduccion

---
+++ app/Http/Controllers/GastosProduccionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GastosProduccion;
+use Illuminate\Http\Request;
+
+class GastosProduccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GastosProduccion  $gastosProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GastosProduccion $gastosProduccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GastosProduccion  $gastosProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GastosProduccion $gastosProduccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GastosProduccion  $gastosProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GastosProduccion $gastosProduccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GastosProduccion  $gastosProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GastosProduccion $gastosProduccion)
+    {
+        //
+    }
+}
