4a0bfc5ba493da53ddf3c6ea2d65a4700f7912c2
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:29:41 -0600

    Se agregaron los atributos de la tabla compras y se agregó la relación a la tabla detalle_compras

--- app/Compra.php
+++ app/Compra.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compra extends Model
 {
-    //
+    protected $table = 'compras';
+
+    protected $fillable = ['fecha', 'total'];
+
+    public function detalleCompra()
+    {
+        return $this->hasMany('App\DetalleCompra');
+    }
 }
