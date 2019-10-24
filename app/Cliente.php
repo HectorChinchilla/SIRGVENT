ca62b93c9d53640ac55d3fe803e8afe13ee43919
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:24:57 -0600

    Se agregaron los atributos de la tabla clientes y se agregó la relación a la tabla ventas

--- app/Cliente.php
+++ app/Cliente.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $table = 'clientes';
+
+    protected $fillable = ['nombre_completo', 'direccion', 'correo_electronico', 'telefono', 'descripcion', 'estado_actual'];
+
+    public function venta()
+    {
+        return $this->hasMany('App\Venta');
+    }
 }
