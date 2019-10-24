ace9bcac6e453205d53c59319a4b575d01819b86
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 13:56:34 -0600

    Se agregaron los atributos a la migración detalle_compras y se agregaron las llaves foreanas de las tablas compras y productos

--- database/migrations/2019_10_21_221335_create_detalle_compras_table.php
+++ database/migrations/2019_10_21_221335_create_detalle_compras_table.php
@@ -15,7 +15,15 @@ class CreateDetalleComprasTable extends Migration
     {
         Schema::create('detalle_compras', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('cantidad');
+            $table->decimal('precio', 11, 2);
+            $table->decimal('subtotal', 11, 2);
+            $table->bigInteger('compra_id')->unsigned();
+            $table->bigInteger('producto_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('compra_id')->references('id')->on('compras');
+            $table->foreign('producto_id')->references('id')->on('productos');
         });
     }
 
