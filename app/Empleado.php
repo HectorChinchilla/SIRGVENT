6446c46e259f934c332809c845d41d7cd1ef3872
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:49:34 -0600

    Se agregaron los atributos de la tabla empleados y se agregó la relación a la tabla ventas y la relación a la tabla posicion_empleados

--- app/Empleado.php
+++ app/Empleado.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleado extends Model
 {
-    //
+    protected $table = 'empleados';
+
+    protected $fillable = ['nombre_completo', 'direccion', 'correo_electronico', 'telefono',  'c_u_i', 'fecha_nacimiento', 'estado_actual', 'posicion_empleado_id'];
+
+    public function ventas()
+    {
+        return $this->hasMany('App\Venta');
+    }
+
+    public function posicionEmpleado()
+    {
+        return $this->belongsTo('App\PosicionEmpleado');
+    }
 }
