b22efc54862c6a8fca46a81c93b8495f94856740
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:36:45 -0600

    Se agregaron los atributos de la tabla detalle_compras y se agregó la relación a la tabla compras y la relación a la tabla productos

--- app/DetalleCompra.php
+++ app/DetalleCompra.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetalleCompra extends Model
 {
-    //
+    protected $table = 'detalle_compras';
+
+    protected $fillable = ['cantidad', 'precio', 'subtotal', 'compra_id', 'producto_id'];
+
+    public function compra()
+    {
+        return $this->belongsTo('App\Compra');
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo('App\Producto');
+    }
 }
