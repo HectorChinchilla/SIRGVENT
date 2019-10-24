309fd060efb4d98e3bb8faccd7092cdf68013333
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 17:41:51 -0600

    Se creó el DatabaseSeeder en donde llamamos a todos los seeders que creamos anteriormente

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ClientesTableSeeder::class);
+        $this->call(ProductosTableSeeder::class);
+        $this->call(PosicionEmpleadosTableSeeder::class);
+        $this->call(EmpleadosTableSeeder::class);
+        $this->call(VentasTableSeeder::class);
+        $this->call(DetalleVentasTableSeeder::class);
+        $this->call(ComprasTableSeeder::class);
+        $this->call(DetalleComprasTableSeeder::class);
+        $this->call(GastosProduccionTableSeeder::class);
     }
 }
