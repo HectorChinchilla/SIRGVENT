c245e8df53ec55558c17fd48ec6efc0a82f5ff3e
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 13:03:10 -0600

    Se agregaron los atributos de la tabla ventas y se agregó la relación a la tabla clientes, la relación a la tabla detalle_ventas y la relación a la tabla empleados

--- app/Venta.php
+++ app/Venta.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
-    //
+    protected $table = 'ventas';
+
+    protected $fillable = ['fecha', 'total', 'descripcion', 'cliente_id', 'empleado_id'];
+
+    public function cliente()
+    {
+        return $this->belongsTo('App\Cliente');
+    }
+
+    public function detalleVenta()
+    {
+
+        return $this->hasMany('App\DetalleVenta');
+    }
+
+    public function empleado()
+    {
+        return $this->belongsTo('App\Empleado');
+    }
 }
