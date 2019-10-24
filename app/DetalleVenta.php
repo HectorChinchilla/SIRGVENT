48d357dfc77ab6cf981578956afc71e024c37b45
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:42:31 -0600

    Se agregaron los atributos de la tabla detalle_ventas y se agregó la relación a la tabla ventas y la relación a la tabla productos

--- app/DetalleVenta.php
+++ app/DetalleVenta.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetalleVenta extends Model
 {
-    //
+    protected $table = 'detalle_ventas';
+
+    protected $fillable = ['cantidad', 'precio', 'subtotal', 'venta_id', 'producto_id'];
+
+
+    public function venta()
+    {
+        return $this->belongsTo('App\Venta');
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo('App\Producto');
+    }
 }
