fccbbe163053f6adc9a5ccb7eaedf04df95f3560
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:52:32 -0600

    Se agregaron los atributos de la tabla gastos_produccions y se agregó la relación a la tabla productos

--- app/GastosProduccion.php
+++ app/GastosProduccion.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class GastosProduccion extends Model
 {
-    //
+    protected $table = 'gastos_produccions';
+
+    protected $fillable = ['nombre', 'precio', 'descripcion', 'producto_id'];
+
+    public function producto()
+    {
+        return $this->belongsTo('App\Producto');
+    }
 }
