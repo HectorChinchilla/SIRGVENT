1ebb0a03cebd46f1c858baefc439df23535efc58
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:54:33 -0600

    Se agregaron los atributos de la tabla posicion_empleados y se agregó la relación a la tabla empleados

--- app/PosicionEmpleado.php
+++ app/PosicionEmpleado.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PosicionEmpleado extends Model
 {
-    //
+    protected $table = 'posicion_empleados';
+
+    protected $fillable = ['nombre', 'descripcion'];
+
+    public function empleado()
+    {
+        return $this->hasMany('App\Empleado');
+    }
 }
