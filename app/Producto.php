33aae2ebf2bc349091e67a2c3baeb17f19683af2
HectorChinchilla <hectorguzman.lg@gmail.com>
2019-10-24 12:59:43 -0600

    Se agregaron los atributos de la tabla productos y se agregó la relación a la tabla detalle_ventas, la relación a la tabla detalle_compras y la relación a la tabla gastos_produccions

--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $table = 'productos';
+
+    protected $fillable = ['nombre_completo', 'cantidad', 'precio_compra', 'precio_venta', 'tiempo_produccion', 'estado_actual'];
+
+
+    public function detalleVenta()
+    {
+        return $this->hasMany('App\DetalleVenta');
+    }
+
+    public function detalleCompra()
+    {
+        return $this->hasMany('App\DetalleCompra');
+    }
+
+    public function gastosProduccion()
+    {
+        return $this->hasMany('App\GastosProduccion');
+    }
 }
